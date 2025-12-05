668e2d204c46327d16d901e0d60ef1fe2128df43
Iterasys <contato@iterasys.com>
2025-12-05 11:57:15 -0300

    Post DDT

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -6,7 +6,7 @@ const petId = 1001
 describe('API PetStore Swagger - Entidade Pet', () => {
     // Atributos, objetos, constantes e variáveis
     const request = supertest('https://petstore.swagger.io/v2') // Base URL
-
+    const massaPet = require('../../vendors/json/massaPet')
     // Funções e métodos = it
     // Incluir animal
     it('POST Pet', () => {
@@ -62,5 +62,57 @@ describe('API PetStore Swagger - Entidade Pet', () => {
             })
 
     }) // Final do Delete
+
+    // POST Pet DDT - Utilizando massa de teste
+    it.each(massaPet.array.map(elemento => [
+        elemento.id,
+        elemento.categoryid,
+        elemento.categoryname,
+        elemento.petname,
+        elemento.tag0id,
+        elemento.tag0name,
+        elemento.tag1id,
+        elemento.tag1name, 
+        elemento.petstatus
+    ]))
+    ('POST Pet Data Driven: %s', (
+        id,
+        categoryid,
+        categoryname,
+        petname,
+        tag0id,
+        tag0name,
+        tag1id,
+        tag1name, 
+        petstatus
+    ) => {
+        // Modelo do json a ser enviado
+        // Valores que não forem sobrepostos seguirão como no modelo
+        const pet = require('../../vendors/json/pet.json')
+
+        // Sobreposição de valores
+        pet.id = id
+        pet.category.id = categoryid
+        pet.category.name = categoryname
+        pet.name = petname
+        pet.tags[0].id = tag0id
+        pet.tags[0].name = tag0name
+        pet.tags[1].id = tag1id
+        pet.tags[1].name = tag1name
+        pet.status = petstatus
+
+        // Teste em si
+        return request
+            .post('/pet')
+            .send(pet)
+            .then((response) => {
+                expect(response.statusCode).toBe(200)
+                expect(response.body.id).toBe(id)
+                expect(response.body.name).toBe(petname)
+                expect(response.body.category.name).toBe(categoryname)
+                expect(response.body.tags[0].name).toBe(tag0name)
+            })
+
+    }) // Final do Post Pet DDT
 
 })
